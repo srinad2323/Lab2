4d14a0453266c3b2e6fa671a19fe2e1a0881af36
srinad2323 <larafun42@gmail.com>
2026-02-26 10:22:12 -0500

    Add files via upload

---
+++ hamming_dist.s
@@ -0,0 +1,40 @@
+.section .data
+str1: .ascii "this is a test"
+str2: .ascii "of the emergency broadcast"
+str1len = . - str1
+
+.section .text
+.globl hamming_distance
+
+hamming_distance:
+    xor %eax, %eax
+    mov $str1, %rsi
+    mov $str2, %rdi
+    mov $str1len, %ecx
+
+hamming_loop:
+    xor %edx, %edx
+
+    movb (%rsi), %dl
+    movb (%rdi), %bl
+    xor  %bl, %dl
+
+    mov $8, %ebx
+
+hamming_bit_loop:
+    shr $1, %dl
+    adc $0, %edx
+    dec %ebx
+    jnz hamming_bit_loop
+
+    add %edx, %eax
+
+    add $1, %rsi
+    add $1, %rdi
+
+    dec %ecx
+    jnz hamming_loop
+
+    ret
+    
+.section .note.GNU-stack,"",@progbits
