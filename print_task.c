f6b97c9e98375cf57820a66a6987987593e944b7
srinad2323 <larafun42@gmail.com>
2026-02-26 14:58:29 -0500

    Update print_task.c

--- print_task.c
+++ print_task.c
@@ -3,7 +3,7 @@
 extern int hamming_distance(void);
 int main(){
     int result = hamming_distance();
-    printf("this is test\n");
+    printf("this is a test\n");
     printf("of the emergency broadcast\n");
     printf("Hamming distance: %d\n", result);
     return 0;
